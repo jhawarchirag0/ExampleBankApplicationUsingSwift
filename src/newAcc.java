77b71ccb89b39523149ac0798f3de3633f09f4c8
chirag jhawar <chirag.jhavar@somaiya.edu>
2020-05-12 18:39:35 +0530

    First commit

---
+++ src/newAcc.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jhawa
+ */
+public class newAcc{
+        String prefix;
+        String firstName;
+        String lastName;
+        String accountType;        
+        String gender;
+        String month;
+        int date;
+        int year;
+        String contact;
+        String email;
+        String occupation;
+        String ad1;
+        String ad2;
+        String city;
+        String state;
+        String postalCode;
+        String maritial;
+        String aadharCard;
+        String accNo;
+        
+    newAcc(String prefix,
+        String firstName,
+        String lastName,
+        String accountType,        
+        String gender,
+        String month,
+        int date,
+        int year,
+        String contact,
+        String email,
+        String occupation,
+        String ad1,
+        String ad2,
+        String city,
+        String state,
+        String postalCode,
+        String maritial,
+        String aadharCard,
+        String accNo){
+        
+        this.prefix = prefix;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountType = accountType;
+        this.gender = gender;
+        this.month = month;
+        this.date = date;
+        this.year = year;
+        this.contact = contact;
+        this.email = email;
+        this.occupation = occupation;
+        this.ad1 = ad1;
+        this.ad2 = ad2;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+        this.maritial = maritial;
+        this.aadharCard = aadharCard;
+        this.accNo = accNo;
+    }
+}
